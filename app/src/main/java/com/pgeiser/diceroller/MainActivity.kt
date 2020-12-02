a55f492971b237480e5a206a0283ee602c06eb7e
13pgeiser <pgeiser@pgeiser.com>
2020-12-02 15:46:03 +0100

    Update Textview on button clic

--- app/src/main/java/com/pgeiser/diceroller/MainActivity.kt
+++ app/src/main/java/com/pgeiser/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pgeiser.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
-            Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
     }
 }
